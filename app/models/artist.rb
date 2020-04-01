7e4b717396d2cec0c980b28b66f8991c9550ea99
emanningt <emanningmusic@yahoo.com>
2020-04-01 09:19:21 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+  def song_count
+    self.songs.count
+  end
+
+  def pluralize_song
+    'song'.pluralize(self.song_count)
+    end 
 end
